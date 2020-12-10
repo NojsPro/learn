053cefc3ba8706d818dd3fcb77c7f5f669fea83e
luo <811822704@qq.com>
2020-12-10 17:54:09 +0800

    同步

--- vue/source_code/compile.js
+++ vue/source_code/compile.js
@@ -1,3 +1,18 @@
+class VNode {
+    constructor (tag, data, children, text, elm){
+        /* 当前节点的标签名 */
+        this.tag = tag;
+        /* 当前节点的一些数据信息，比如props、attrs等数据 */
+        this.data = data;
+        /* 当前节点的子节点，是一个数组 */
+        this.children = children;
+        /* 当前节点的文本 */
+        this.text = text;
+        /* 当前虚拟节点对用的真实dom节点 */
+        this.elm = elm;
+    }
+}
+
 /* 匹配任意字符 */
 const ncname = '[a-zA-Z_][\\w\\-\\.]*';
 /* [^\s"'<>/=] 至少有一个这样的字符 */
@@ -389,6 +404,48 @@ function generate (rootAst) {
     return {
         render: `with(this){return ${code}}`
     }
+}
+
+
+
+function render () {
+    return new VNode(
+        'span',
+        /* 指令集合数组 */
+        {
+            directives:[{
+                /* v-show指令 */
+                rawName: 'v-show',
+                expression: 'isShow',
+                name: 'show',
+                value: true
+            }],
+            /* 静态class */
+            staticClass: 'demo'
+        },
+        [new VNode(undefined, undefined, undefined, 'This is a span.', undefined)]
+    )
+}
+/* 创建空节点 */
+function createEmptyVNode () {
+    const node = new VNode();
+    node.text = '';
+    return node;
+}
+/* 创建文本节点 */
+function createTextVNode (val) {
+    return new VNode(undefined, undefined, undefined, String(val));
+}
+/* 克隆节点 */
+function cloneVNode (node) {
+    const clone_node = new VNode(
+        node.tag,
+        node.data,
+        node.children,
+        node.text,
+        node.elm
+    )
+    return clone_node;
 }
 
 parseHTML()
